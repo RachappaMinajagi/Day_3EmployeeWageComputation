3b0e73cf361e08ed07b518d199c5680dbcfae791
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-02-16 21:11:30 +0530

    Solving Using the Switch case Statement

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -2,26 +2,37 @@
 import java.util.Random;
 
 
-public class EmpWage {
-    public static void CheEmpStatus(int num){
+import java.util.Random;
+
+
+public class Main {
+    public static void EmpWageStatus(int num){
         int Wage_HR = 20;
         int Day_HRs = 8;
-        if(num == 1){
-            System.out.println("Employee Present");
-            System.out.println("DayWage = " + Wage_HR * Day_HRs);
+        int Part_HRs = 8;
+
+        switch (num){
+            case 1:
+                System.out.println("Employee Present");
+                System.out.println("DayWage = " + Wage_HR * Day_HRs);
+                break;
+            case 2:
+                System.out.println("Employee Present");
+                System.out.println("DayWage with Part Time = " + Wage_HR * (Day_HRs + Part_HRs));
+                break;
+            case 0:
+                System.out.println("Employee Absent");
+                System.out.println("DayWage = "+0);
         }
-        else{
-            System.out.println("Employee Absent");
-            System.out.println("DayWage = " +0);
-        }
+
 
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         Random random = new Random();
-        int r = random.nextInt(2);
-        CheEmpStatus(r);
+        int r = random.nextInt(3);
+        EmpWageStatus(r);
 
     }
 }
