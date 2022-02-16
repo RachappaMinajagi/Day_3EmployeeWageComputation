3cd4f828df6a26d0ffeb95721a6e2747c24c73c6
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-02-16 21:41:54 +0530

    Employee Wage Computation Program

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -2,26 +2,39 @@
 import java.util.Random;
 
 
-public class EmpWage {
-    public static void CheEmpStatus(int num){
+public class Main {
+    public static void EmpWageStatus(int num){
         int Wage_HR = 20;
         int Day_HRs = 8;
-        if(num == 1){
-            System.out.println("Employee Present");
-            System.out.println("DayWage = " + Wage_HR * Day_HRs);
+        int Part_HRs = 8;
+        int NumDays = 20;
+        double daywage = 0;
+        double monthwage = 0;
+
+        for (int i =0; i<NumDays; i++){
+            switch (num){
+                case 1:
+                    daywage = Wage_HR * Day_HRs * i;
+                    break;
+                case 2:
+                    daywage = Wage_HR * (Day_HRs + Part_HRs) * i;
+                    break;
+                case 0:
+                    break;
+
+            }
+            monthwage = monthwage + daywage;
         }
-        else{
-            System.out.println("Employee Absent");
-            System.out.println("DayWage = " +0);
-        }
+        System.out.println("Month Wage : " + monthwage);
+
 
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         Random random = new Random();
-        int r = random.nextInt(2);
-        CheEmpStatus(r);
+        int r = random.nextInt(3);
+        EmpWageStatus(r);
 
     }
 }
