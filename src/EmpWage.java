8ecbf5b4ea7856cf6be6c89ad79faa1c44e4ac3b
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-02-16 17:11:56 +0530

    UC_2 Daily Wages

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,8 +1,27 @@
 
+import java.util.Random;
+
+
 public class EmpWage {
+    public static void CheEmpStatus(int num){
+        int Wage_HR = 20;
+        int Day_HRs = 8;
+        if(num == 1){
+            System.out.println("Employee Present");
+            System.out.println("DayWage = " + Wage_HR * Day_HRs);
+        }
+        else{
+            System.out.println("Employee Absent");
+            System.out.println("DayWage = " +0);
+        }
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to employee wage computation program");
-	}
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        Random random = new Random();
+        int r = random.nextInt(2);
+        CheEmpStatus(r);
+
+    }
 }
